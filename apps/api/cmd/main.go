20e2fcbe263b4c7e9fd6c6578a94ddae4d007064
fabs <fabiant941@gmail.com>
2026-05-01 21:20:26 -0700

    fix(api): error discarded on listen and serve

--- apps/api/cmd/main.go
+++ apps/api/cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -40,5 +41,7 @@ func main() {
 		r.Post("/logout", authHandler.Logout)
 	})
 
-	http.ListenAndServe(":"+cfg.Port, r)
+	if err := http.ListenAndServe(":"+cfg.Port, r); err != nil {
+		log.Fatal(err)
+	}
 }
